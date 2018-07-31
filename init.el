9694477a7ff48eb68f18c640a37d4047a359dc3e
Jacob <jac21934@vt.edu>
2018-07-31 16:03:14 -0600

    added better mutlicursor stuff

--- init.el
+++ init.el
@@ -2,21 +2,18 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") )
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
+;;(package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
 	(package-refresh-contents)
-	(package-install 'use-package))
-(package-initialize)
+	(package-install 'use-package)
+	)
+;;(package-initialize)
 
-
-
-;; Ensure org is installed)
+;; Ensure org is installed
 (use-package org
 	:ensure org-plus-contrib)
 
 ;; Actually load init file
 (org-babel-load-file (expand-file-name "~/.emacs.d/configuration.org"))
-
-
